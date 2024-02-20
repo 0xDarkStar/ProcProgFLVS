f5cf36c95d3e388854037397c825d464ca3d95cf
0xDarkStar <mattrojas.email@gmail.com>
2024-02-19 19:00:49 -0500

    Barely added logic

--- Semester2/406Project/registration.py
+++ Semester2/406Project/registration.py
@@ -45,10 +45,10 @@ def appMode():
 class popFrames():
     def popControlFrame():
         # Populate the control frame with buttons and text inputs
-        removeUser = Button(controlFrame, text="Remove User", padx=10, pady=10, command=kill)
-        addUser = Button(controlFrame, text="Add User", padx=10, pady=10, command=kill)
-        outputList = Button(controlFrame, text="View User List", padx=10, pady=10, command=kill)
-        outputUser = Button(controlFrame, text= "View User Info", padx=10,pady=10, command=kill)
+        removeUser = Button(controlFrame, text="Remove User", padx=10, pady=10, command=commands.delUser)
+        addUser = Button(controlFrame, text="Add User", padx=10, pady=10, command=commands.addUser)
+        outputList = Button(controlFrame, text="View User List", padx=10, pady=10, command=commands.outList)
+        outputUser = Button(controlFrame, text= "View User Info", padx=10,pady=10, command=commands.outUser)
         # Load them all
         removeUser.grid(row=0, column=0)
         addUser.grid(row=0, column=1)
@@ -57,10 +57,35 @@ class popFrames():
 
     def popOutputFrame():
         print("Populating output frame")
+        userList = Listbox(outputFrame)
+        userInfo = Listbox(outputFrame)
     
 class commands():
-    def delUser(playerID):
+    def delUser():
         print()
+    def addUser():
+        print()
+    def outList():
+        userInfoCommands.searchUserList()
+    def outUser():
+        print()
+
+class userInfoCommands():
+    def searchUserList():
+        with open("battle_royale.csv") as file:
+            table = csv.reader(file, delimiter=',')
+            rows = [] # To store all the rows for later use
+            for row in table:
+                index = 0
+                for i in row:
+                    if index == 2: # Is at the end of the list
+                        i = i[2:-2] # To remove the ']
+                    else:
+                        i = i[2:-1] # To remove the '
+                    row[index] = i.lower() # Make it lowercase to have it work with the code
+                    index += 1
+                rows += [row]
+        print(rows)
 
 def kill(e):
     mainWindow.destroy()
